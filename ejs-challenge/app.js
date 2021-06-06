4d5006134da7e95d078f537c25a019625e804b7a
Soumyajit Roy <soumyajitroy2411@gmail.com>
2021-06-07 01:40:22 +0530

    completed

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const ejs = require("ejs");
+var _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -16,6 +17,38 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+let posts = [];
+app.get("/", (req, res) => {
+  res.render("home", { Content: homeStartingContent, posts: posts });
+});
+app.get("/about", (req, res) => {
+  res.render("about", { Content: aboutContent });
+});
+app.get("/contact", (req, res) => {
+  res.render("contact", { Content: contactContent });
+});
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
+app.post("/compose", (req, res) => {
+  var post = {
+    titles: req.body.title,
+    content: req.body.body,
+  };
+  posts.push(post);
+  res.redirect("/");
+});
+
+app.get("/posts/:name", function (req, res) {
+  const rtitle = _.lowerCase(req.params.name);
+  posts.forEach(function (post) {
+    const store = _.lowerCase(post.titles);
+    if (store === rtitle) {
+      res.render("post", { title: post.titles, content: post.content });
+    }
+  });
+});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
