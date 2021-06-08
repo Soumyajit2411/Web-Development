0b9a7baa4b0fc6dffe2d41f44c16a4fccdf67a2c
Soumyajit Roy <soumyajitroy2411@gmail.com>
2021-06-08 12:49:02 +0530

    pro

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const ejs = require("ejs");
 var _ = require("lodash");
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -18,9 +20,23 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let posts = [];
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const postSchema = new Schema({
+  title: String,
+  content: String,
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+// let posts = [];
 app.get("/", (req, res) => {
-  res.render("home", { Content: homeStartingContent, posts: posts });
+  Post.find({}, function (error, posts) {
+    res.render("home", { startingContent: homeStartingContent, posts: posts });
+  });
 });
 app.get("/about", (req, res) => {
   res.render("about", { Content: aboutContent });
@@ -32,24 +48,30 @@ app.get("/compose", (req, res) => {
   res.render("compose");
 });
 app.post("/compose", (req, res) => {
-  var post = {
-    titles: req.body.title,
-    content: req.body.body,
-  };
-  posts.push(post);
-  res.redirect("/");
+  // var post = {
+  //   titles: req.body.title,
+  //   content: req.body.body,
+  // };
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  });
+  post.save(function (err) {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
 });
 
-app.get("/posts/:name", function (req, res) {
-  const rtitle = _.lowerCase(req.params.name);
-  posts.forEach(function (post) {
-    const store = _.lowerCase(post.titles);
-    if (store === rtitle) {
-      res.render("post", { title: post.titles, content: post.content });
-    }
+app.get("/posts/:postId", function (req, res) {
+  const requestedPostId = req.params.postId;
+  Post.findOne({ _id: requestedPostId }, function (error, post) {
+    res.render("post", { title: post.title, content: post.content });
   });
 });
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
+
+// mongodb+srv://dexter:soumyajit@soumyajit.rz4cw.mongodb.net/blogDB
